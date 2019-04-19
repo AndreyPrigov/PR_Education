1c457e396480bcd14ecf159b0da04a259e30c056
AndreyPrigov <andreyprigov@gmail.com>
2019-04-19 18:30:51 -0400

    initial2

---
+++ map.js
@@ -0,0 +1,38 @@
+'use strict'
+
+console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmV5cHJpZ292IiwiYSI6ImNqc280aHdoajBqMzY0NG41ODRwOG0wZnAifQ.9UHLZRigw6UorfQZwQe4uw'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/andreyprigov/cjundmg6paf071fqdrbsjg9aw',
+    center: [-66.427494,18.008719],
+    zoom: 8
+})
+
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+map.addControl(navigation, 'top-left')
+
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+map.addControl(geolocate, 'top-left')
+
+geolocate.on('geolocate', function(event) {
+
+})
